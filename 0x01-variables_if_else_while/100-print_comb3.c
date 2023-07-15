7e4d2afd83bf046fa1784d7a06393a7071e960af
Ahmed <ahmed.hamed011127@gmail.com>
2023-07-15 05:51:23 -0700

    a

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Description: print all possible different
+*
+*Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int x = 0, y;
+
+	while (x <= 9)
+	{
+		y = 0;
+		while (y <= 9)
+		{
+			if (x != y && x < y)
+			{
+				putchar(x + 48);
+				putchar(y + 48);
+
+				if (x + y != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			y++;
+		}
+		x++;
+	}
+	putchar('\n');
+
+	return (0);
+}
