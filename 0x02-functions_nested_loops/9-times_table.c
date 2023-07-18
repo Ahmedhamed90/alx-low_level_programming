fc101c5ae2d39ad83635199c5b432b247e8c7b7d
Ahmed <ahmed.hamed011127@gmail.com>
2023-07-18 12:05:01 -0700

    a

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,11 +18,15 @@ void times_table(void)
 
 			d = a * s;
 
+			/*
+			 * put space if product is a single number
+			 * place the first digit if its two numbers
+			*/
 			if (d <= 9)
 				_putchar(' ');
 			else
-				_putchar((d / 10) + 48);
-			_putchar((d % 10) + 48);
+				_putchar((d / 10) + 48); /*get the first digit*/
+			_putchar((d % 10) + 48); /*get the secand digit*/
 		}
 		_putchar('\n');
 	}
