0d2ae71378e4652ccb31b69b60c93c6a5687de1b
Ahmed <ahmed.hamed011127@gmail.com>
2023-07-18 08:06:03 -0700

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * * main - Entry point
+ *
+ * Description: print _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
