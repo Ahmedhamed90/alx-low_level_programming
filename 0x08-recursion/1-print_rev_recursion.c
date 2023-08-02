889a35171ad9412cdcbeb6ae65f9588c31ff7099
Ahmed <ahmed.hamed011127@gmail.com>
2023-08-02 05:07:21 -0700

    a

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - pointer
+ *
+ * @s: pointer a string
+ *
+ * Return: void
+*/
+
+void _print_rev_recursion(char *s)
+{
+	string x = "";
+
+	for (int z = *s.length() - 1 ; z >= 0; z--)
+	{
+		x += *s[z]
+	}
+}
