b459e13d513212f21d69eece98537d4bf8afe29f
Ahmed <ahmed.hamed011127@gmail.com>
2023-08-02 05:49:07 -0700

    a

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _factorial - pointer
+ *
+ * @n: int
+ *
+ * Return: int
+*/
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
