05dcd493f28127fe5e6b962975dcaa5ad36d58bb
Ahmed <ahmed.hamed011127@gmail.com>
2023-08-02 08:56:03 -0700

    a

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,11 +1,40 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * is_prime_number - pointer int
  *
  * @n: int
+ * @x: int
  *
- * Return: int
+ * Return: 0 or 1
 */
 
+int check(int n, int x);
 int is_prime_number(int n)
+{
+	return (check(n, 2));
+}
+
+/**
+ * is_prime_number - pointer int
+ *
+ * @n: int
+ * @x: int
+ *
+ * Return: 0 or 1
+*/
+
+int check(int n, int x)
+{
+	if (x >= n && n > 1)
+	{
+		return (1);
+	}
+	else if (n % x == 0 || n <= 1)
+	{
+		return (0);
+	}
+	else
+		return (check(n, x + 1));
+}
