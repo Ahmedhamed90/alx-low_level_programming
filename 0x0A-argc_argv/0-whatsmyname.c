e512c2a0cf26541abe75d8df50c820a1f318d015
Ahmed <ahmed.hamed011127@gmail.com>
2023-08-04 07:41:36 -0700

    a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main pointer program name
+ *
+ * @argc: int
+ * @argv: char
+ *
+ * Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
