58f7b6ab7826ea594fbaf7cdeaebf686f864ce12
Ahmed <ahmed.hamed011127@gmail.com>
2023-08-07 05:24:17 -0700

    a

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * free_grid - free the grid
+ *
+ * @grid: grid of memories
+ * @height: int
+ *
+ * Return: void
+*/
+
+void free_grid(int **grid, int height)
+{
+	int x = 0;
+
+	for (x < height; x++)
+		free(grid[x]);
+	ree(grid);
+}
