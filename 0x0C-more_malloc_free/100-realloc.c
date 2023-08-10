a7a128d88b961f757977aa2fd3cc2f15c94e4c52
Ahmed <ahmed.hamed011127@gmail.com>
2023-08-10 06:52:15 -0700

    a

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,7 +15,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	void *p;
 	unsigned int i;
 
-	if (new_sie == old_size)
+	if (new_size == old_size)
 		return (ptr);
 	if (new_size == 0 && ptr != NULL)
 	{
