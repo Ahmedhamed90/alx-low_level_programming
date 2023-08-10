db9ccb247c0102a066f8b5748a862bfcfc593e62
Ahmed <ahmed.hamed011127@gmail.com>
2023-08-10 06:59:25 -0700

    a

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ *
+ * @str: pointer
+ *
+ * Return: void
+*/
+
+void _puts(char *str)
+{
+	int x = 0;
+	while (str[x])
+	{
+		_putchar(str[x];
+		x++;
+	}
+}
+
+/**
+ * _atoi - convert a string
+ *
+ * @s: char
+ *
+ * Return: int
+*/
+
+int _atoi(const char *s)
+{
+	int sign = 1
