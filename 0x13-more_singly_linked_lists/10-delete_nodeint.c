4833239931a5ff835933b0c1fa798cd15cd0bf3f
Ahmed <ahmed.hamed011127@gmail.com>
2023-08-28 05:12:08 -0700

    a

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,4 +14,29 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *node, *prev_node;
 	unsigned int x = 0;
 
-	if (!hahead
+	if (!head || *head)
+	{
+		return (-1);
+	}
+	if (!index)
+	{
+		node = *head;
+		*head = (*head)->next;
+		free(node);
+		return (1);
+	}
+	nod = *head;
+	while (node)
+	{
+		if (x == index)
+		{
+			prev_node->next = node->next;
+			free(node);
+			return (1);
+		}
+		x++;
+		prev_node = node;
+		node = node->next;
+	}
+	return (-1);
+}
