5e42c82ca702fc4f32f6111a4e869c817ee2aac8
Ahmed <ahmed.hamed011127@gmail.com>
2023-08-28 03:56:53 -0700

    alx

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - print
+ * @h: pointer
+ * Return: size
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t x = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		x++;
+	}
+	return (x);
+}
