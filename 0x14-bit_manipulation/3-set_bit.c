d9f8963a7a267b1a2034115464952756d97ee36d
Ahmed <ahmed.hamed011127@gmail.com>
2023-08-31 07:52:11 -0700

    a

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the bit
+ * @n: number
+ * @index: bit
+ * Return: 1 if successful
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return(-1);
+
+	return (!!(*n |= 1L << index));
+}
