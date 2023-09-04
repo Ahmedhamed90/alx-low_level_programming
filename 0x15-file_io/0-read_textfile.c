c3a9d7a149062581927c19c1711932fedebd7264
Ahmed <ahmed.hamed011127@gmail.com>
2023-09-04 05:28:47 -0700

    a

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * read_textfile - read_textfile
+ *
+ * @filename: name of file
+ * @letters: number
+ *
+ * Return: number
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t bytes;
+	char but[READ_BUF_SIZE * 8];
+
+	if (!filename || !letters)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	bytes = read(fd, &buf[0], leters);
+	bytes = write(STDOUT_FILENO, &but[0], bytes);
+	close(fd);
+	return (bytes);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+this is read me for 0x15-file_io
